41ae074978b827662077c2b39c61d1d9ba8ea0dc
Michel Ho <mho85@icloud.com>
2019-11-03 22:19:04 +0100

    Init projet

---
+++ js/Class.js
@@ -0,0 +1,90 @@
+class Component {
+	constructor(x, y, w, h, src) {
+		const img = document.createElement("img");
+		img.onload = () => {
+			this.img = img;
+
+			this.w = w;
+			this.h = h;
+
+			this.x = x;
+			this.y = y;
+		};
+		img.src = src;
+	}
+
+	draw() {
+		if (!this.img) return; // if `this.img` is not loaded yet => don't draw
+		ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
+	}
+}
+
+class Frogger extends Component {
+	constructor() {
+		super(63, H - h, 63, 70, "img/frogger.png");
+	}
+	moveLeft() {
+		this.x += -this.w;
+	}
+	moveRight() {
+		this.x += this.w;
+	}
+	moveUp() {
+		this.y += -this.h;
+	}
+	moveDown() {
+		this.y += this.h;
+	}
+}
+
+class CarLeft extends Component {
+	constructor() {
+		super(-153, 0.7 * H, 153, 70, "img/car_from_left.png");
+	}
+
+	hits(person) {
+		return (
+			Math.abs(this.x - person.x) <= this.w &&
+			Math.round(this.y) === person.y
+		);
+	}
+}
+
+class TruckLeft extends Component {
+	constructor() {
+		super(-141, 0.8 * H, 141, 70, "img/truck_from_left.png");
+	}
+
+	hits(person) {
+		return (
+			Math.abs(this.x - person.x) <= this.w &&
+			Math.round(this.y) === person.y
+		);
+	}
+}
+
+class CarRight extends Component {
+	constructor() {
+		super(W, 0.5 * H, 153, 70, "img/car_from_right.png");
+	}
+
+	hits(person) {
+		return (
+			Math.abs(this.x - person.x) <= person.w &&
+			Math.round(this.y) === person.y
+		);
+	}
+}
+
+class TruckRight extends Component {
+	constructor() {
+		super(W, 0.6 * H, 141, 70, "img/truck_from_right.png");
+	}
+
+	hits(person) {
+		return (
+			Math.abs(this.x - person.x) <= person.w &&
+			Math.round(this.y) === person.y
+		);
+	}
+}
---
+++ script.js
@@ -0,0 +1,234 @@
+let $canvas = document.getElementById("canvas");
+let ctx = $canvas.getContext("2d");
+const W = canvas.width; // 567
+const H = canvas.height; // 700
+const w = 64;
+const h = 70;
+let frogger, carLeft, carRight, truckLeft, truckRight;
+let lilypad;
+let carsLeft, carsRight, trucksLeft, trucksRight;
+let gameover, win, raf;
+let frames = 0;
+
+function draw() {
+	ctx.clearRect(0, 0, W, H);
+	/* ---- 
+    BACKGROUND
+    ---- */
+	// Chaussée
+	ctx.fillStyle = "rgb(151,151,151)";
+	ctx.fillRect(0, H * 0.9, W, H * 0.1);
+	// Route
+	ctx.fillStyle = "rgb(110,110,110)";
+	ctx.fillRect(0, H * 0.5, W, H * 0.4);
+	// Mare 1
+	ctx.fillStyle = "rgb(223,172,79)";
+	ctx.fillRect(0, H * 0.4, W, H * 0.1);
+	// Eau
+	ctx.fillStyle = "rgb(17,167,254)";
+	ctx.fillRect(0, H * 0.1, W, H * 0.3);
+	// Mare 2
+	ctx.fillStyle = "rgb(223,172,79)";
+	ctx.fillRect(0, 0, W, H * 0.1);
+
+	// Ligne de route 1 - dash
+	ctx.beginPath();
+	ctx.lineWidth = 10;
+	ctx.strokeStyle = "white";
+	ctx.setLineDash([35, 50]);
+	ctx.moveTo(0, H * 0.6);
+	ctx.lineTo(W, H * 0.6);
+	ctx.stroke();
+	ctx.closePath();
+
+	// Ligne de route 2 - trait plein
+	ctx.beginPath();
+	ctx.lineWidth = 10;
+	ctx.strokeStyle = "sandybrown";
+	ctx.setLineDash([]);
+	ctx.moveTo(0, H * 0.7);
+	ctx.lineTo(W, H * 0.7);
+	ctx.stroke();
+	ctx.closePath();
+
+	// Ligne de route 3 - dash
+	ctx.beginPath();
+	ctx.lineWidth = 10;
+	ctx.strokeStyle = "white";
+	ctx.setLineDash([35, 50]);
+	ctx.moveTo(0, H * 0.8);
+	ctx.lineTo(W, H * 0.8);
+	ctx.stroke();
+	ctx.closePath();
+
+	// Nénuphars
+	lilypad = document.querySelector("#lilypad");
+	// Ligne 1 - 3 d'affilée
+	ctx.drawImage(lilypad, 0, 0.3 * H);
+	ctx.drawImage(lilypad, lilypad.width, 0.3 * H);
+	ctx.drawImage(lilypad, 2 * lilypad.width, 0.3 * H);
+	// LIgne 2 - 2 d'affilée
+	ctx.drawImage(lilypad, 3 * lilypad.width, 0.2 * H);
+	ctx.drawImage(lilypad, 4 * lilypad.width, 0.2 * H);
+	// Ligne 3 - 1 sur 2
+	ctx.drawImage(lilypad, 2 * lilypad.width, 0.1 * H);
+	ctx.drawImage(lilypad, 4 * lilypad.width, 0.1 * H);
+	ctx.drawImage(lilypad, 6 * lilypad.width, 0.1 * H);
+
+	// Frogger
+	frogger.draw();
+
+	// Animation
+	// Voitures et camions venant de la gauche
+	if (frames % 200 === 0) {
+		truckRight = new TruckRight();
+		trucksRight.push(truckRight);
+	}
+
+	if (frames % 100 === 0) {
+		carRight = new CarRight();
+		carsRight.push(carRight);
+	}
+
+	if (frames % 150 === 0) {
+		truckLeft = new TruckLeft();
+		trucksLeft.push(truckLeft);
+	}
+
+	if (frames % 50 === 0) {
+		carLeft = new CarLeft();
+		carsLeft.push(carLeft);
+	}
+
+	carsLeft.forEach(function(car) {
+		if (car.x > W) {
+			carsLeft = carsLeft.filter(el => el !== car);
+		}
+		car.x += 10;
+		car.draw();
+	});
+
+	carsRight.forEach(function(car) {
+		if (car.x < -153) {
+			carsRight = carsRight.filter(el => el !== car);
+		}
+		car.x -= 10;
+		car.draw();
+	});
+
+	trucksLeft.forEach(function(truck) {
+		if (truck.x > W) {
+			trucksLeft = trucksLeft.filter(el => el !== truck);
+		}
+		truck.x += 5;
+		truck.draw();
+	});
+
+	trucksRight.forEach(function(truck) {
+		if (truck.x < -141) {
+			trucksRight = trucksRight.filter(el => el !== truck);
+		}
+		truck.x -= 5;
+		truck.draw();
+	});
+
+	for (obstacle of carsLeft) {
+		if (obstacle.hits(frogger)) {
+			console.log("crashed CL");
+			gameover = true;
+			// document.querySelector(".ost-main").pause()
+			// document.querySelector(".ost-gameover").play()
+		}
+	}
+
+	for (obstacle of carsRight) {
+		if (obstacle.hits(frogger)) {
+			console.log("crashed CR");
+			gameover = true;
+			// document.querySelector(".ost-main").pause()
+			// document.querySelector(".ost-gameover").play()
+		}
+	}
+
+	for (obstacle of trucksLeft) {
+		if (obstacle.hits(frogger)) {
+			console.log("crashed TL");
+			gameover = true;
+			// document.querySelector(".ost-main").pause()
+			// document.querySelector(".ost-gameover").play()
+		}
+	}
+
+	for (obstacle of trucksRight) {
+		if (obstacle.hits(frogger)) {
+			console.log("crashed TR");
+			gameover = true;
+			// document.querySelector(".ost-main").pause()
+			// document.querySelector(".ost-gameover").play()
+		}
+	}
+
+	if (frogger.y === 0 && !gameover) {
+		win = true;
+		// document.querySelector(".ost-main").pause()
+		// document.querySelector(".ost-win").play()
+	}
+}
+
+/* ---- 
+MAIN MOVES
+---- */
+document.onkeydown = function(e) {
+	if (!frogger) return;
+
+	console.log("keydown");
+	switch (e.keyCode) {
+		case 37:
+			// left
+			frogger.moveLeft();
+			break;
+		case 38:
+			// up
+			frogger.moveUp();
+			break;
+		case 39:
+			//right
+			frogger.moveRight();
+			break;
+		case 40:
+			// down
+			frogger.moveDown();
+	}
+};
+
+function animLoop() {
+	frames++;
+
+	draw();
+
+	if (!gameover && !win) {
+		raf = requestAnimationFrame(animLoop);
+	}
+}
+
+function startGame() {
+	if (raf) {
+		cancelAnimationFrame(raf);
+	}
+
+	gameover = false;
+	points = 0;
+
+	frogger = new Frogger();
+	carsLeft = [];
+	carsRight = [];
+	trucksLeft = [];
+	trucksRight = [];
+
+	raf = requestAnimationFrame(animLoop);
+	// document.querySelector(".ost-main").play();
+}
+
+document.getElementById("btn-start").onclick = function() {
+	startGame();
+};
